41a10275e7babfb3234c894b900ecdab5c5ace9d
Arpit Agrawal <arpit_agrawal@outlook.com>
2019-03-23 13:52:26 +0530

    First Commit

---
+++ rightclick.js
@@ -0,0 +1,23 @@
+window.onclick = hideContextMenu;
+window.onkeydown = listenKeys;
+var contextMenu = document.getElementById('contextMenu');
+
+function showContextMenu(event) 
+{
+	contextMenu.style.display='block';
+	contextMenu.style.left=event.clientX + 'px';
+	contextMenu.style.top=event.clientY + 'px';
+	return false;
+}
+function hideContextMenu() 
+{
+	contextMenu.style.display='none';
+}
+function listenKeys(event) 
+{
+	
+	var keyCode = event.which || event.keyCode;
+	if (keyCode == 27) {
+	hideContextMenu();
+};
+}		
